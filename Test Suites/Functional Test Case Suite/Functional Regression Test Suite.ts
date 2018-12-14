56bab128025b4a32a9909b3fd2f1b850e8ce555e
Asokan Gunanathan <asokan.gunanathan@rangle.io>
2018-12-14 14:34:18 -0500

    just a commit

--- Test Suites/Functional Test Case Suite/Functional Regression Test Suite.ts
+++ Test Suites/Functional Test Case Suite/Functional Regression Test Suite.ts
@@ -4,7 +4,7 @@
    <name>Functional Regression Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-12-14T13:46:02</lastRun>
+   <lastRun>2018-12-14T14:15:31</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
